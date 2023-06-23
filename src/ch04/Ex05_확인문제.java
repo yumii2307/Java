0bdce5b254d1f44985fca006dbac098b7b1b4a60
Yumi Kim <yumii2307@naver.com>
2023-06-23 17:31:20 +0900

    ch04. 반복문

--- src/ch04/Ex05_확인문제.java
+++ src/ch04/Ex05_확인문제.java
@@ -7,7 +7,7 @@ public class Ex05_확인문제 {
 	public static void main(String[] args) {
 		
 		// 1.
-		int sum = 0;
+		int sum = 0; 
 		for (int i = 1; i <= 100; i++) {
 			if (i % 3 == 0) {
 				sum += i;
