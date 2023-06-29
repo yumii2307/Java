85e077a19fe6cbe5191971d7aa7346210b2b6f8b
Yumi Kim <yumii2307@naver.com>
2023-06-29 14:39:28 +0900

    ch13. 컬렉션 프레임워크

---
+++ src/ch13_collection/sec03_map/Ex03_Properties.java
@@ -0,0 +1,12 @@
+package ch13_collection.sec03_map;
+
+import java.util.Properties;
+
+public class Ex03_Properties {
+
+	public static void main(String[] args) {
+		Properties prop = new Properties();  		// Map<String, String>
+		
+	}
+
+}
