598056c8db742265381c7821082d1b3b2bba7df5
Yumi Kim <yumii2307@naver.com>
2023-06-29 14:58:35 +0900

    ch13. 컬렉션 프레임워크

--- src/ch13_collection/sec03_map/Ex03_Properties.java
+++ src/ch13_collection/sec03_map/Ex03_Properties.java
@@ -1,12 +1,23 @@
 package ch13_collection.sec03_map;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class Ex03_Properties {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Properties prop = new Properties();  		// Map<String, String>
 		
+		// database.properties 파일 로드
+		prop.load(Ex03_Properties.class.getResourceAsStream("database.properties"));
+		
+		String driver = prop.getProperty("driver");
+		System.out.println(driver);
+		
+		System.out.println(prop.getProperty("url"));
+		System.out.println(prop.getProperty("username"));
+		System.out.println(prop.getProperty("password"));
+		System.out.println(prop.getProperty("admin"));			// 홍길동
 	}
 
 }
